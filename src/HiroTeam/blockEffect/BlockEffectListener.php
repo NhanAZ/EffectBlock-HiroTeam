89daec467e3298a8eb5bbe207b353d5766142de1
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-07-06 06:50:08 +0700

    Fix CS

--- src/HiroTeam/blockEffect/BlockEffectListener.php
+++ src/HiroTeam/blockEffect/BlockEffectListener.php
@@ -8,17 +8,17 @@ use pocketmine\entity\EffectInstance;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 
-class BlockEffectListener implements Listener{
+class BlockEffectListener implements Listener {
 
     private $main;
-    public function __construct(BlockEffect $main){
+    public function __construct(BlockEffect $main) {
         $this->main = $main;
     }
 
-    public function onBlock(PlayerMoveEvent $event){
+    public function onBlock(PlayerMoveEvent $event) {
         $player = $event->getPlayer();
 
-        foreach($this->main->config->getAll() as $item => $parametre){
+        foreach ($this->main->config->getAll() as $item => $parametre) {
             $poisonBlock = $parametre['block'];
 
             $block = $player->getLevel()->getBlock($event->getPlayer()->subtract(0, 1, 0));
@@ -28,7 +28,7 @@ class BlockEffectListener implements Listener{
 
             $minute = $effect[1] * 20;
 
-            if($poisonBlock == $block->getId(). ":" .$block->getDamage()){
+            if ($poisonBlock == $block->getId() . ":" . $block->getDamage()) {
 
                 $potionEffect = new EffectInstance(Effect::getEffect($effect[0]), $minute, $effect[2], $parametre['visible']);
                 $player->addEffect($potionEffect);
