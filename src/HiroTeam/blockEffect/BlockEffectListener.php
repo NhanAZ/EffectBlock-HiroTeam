79642d5a9bf5df94a7904f7424aeae27f8d8af0d
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-07-06 07:15:32 +0700

    Why?

--- src/HiroTeam/blockEffect/BlockEffectListener.php
+++ src/HiroTeam/blockEffect/BlockEffectListener.php
@@ -18,19 +18,13 @@ class BlockEffectListener implements Listener {
 
     public function onBlock(PlayerMoveEvent $event) {
         $player = $event->getPlayer();
-
         foreach ($this->main->getConfig()->getAll() as $item => $parametre) {
             $poisonBlock = $parametre['block'];
-
             $block = $player->getWorld()->getBlock($event->getPlayer()->getPosition()->down());
-
             $effect = $parametre['effect'];
             $effect = explode(":", $effect);
-
             $minute = $effect[1] * 20;
-
             if ($poisonBlock == $block->getId() . ":" . $block->getMeta()) {
-
                 $potionEffect = new EffectInstance(EffectIdMap::getInstance()->fromId((int)$effect[0]), $minute, $effect[2], $parametre['visible']);
                 $player->getEffects()->add($potionEffect);
             }
