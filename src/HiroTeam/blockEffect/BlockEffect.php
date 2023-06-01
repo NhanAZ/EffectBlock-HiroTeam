b13d74b11a034bdd6f0a47a18f2ae9468ad18d7a
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-06-02 00:52:33 +0700

    Format Docs

--- src/HiroTeam/blockEffect/BlockEffect.php
+++ src/HiroTeam/blockEffect/BlockEffect.php
@@ -6,11 +6,11 @@ use pocketmine\plugin\PluginBase;
 
 class BlockEffect extends PluginBase {
 
-    protected static $instance;
+	protected static $instance;
 
-    protected function onEnable(): void {
-        self::$instance = $this;
-        $this->getServer()->getPluginManager()->registerEvents(new BlockEffectListener($this), $this);
-        $this->saveDefaultConfig();
-    }
+	protected function onEnable(): void {
+		self::$instance = $this;
+		$this->getServer()->getPluginManager()->registerEvents(new BlockEffectListener($this), $this);
+		$this->saveDefaultConfig();
+	}
 }
--- src/HiroTeam/blockEffect/BlockEffectListener.php
+++ src/HiroTeam/blockEffect/BlockEffectListener.php
@@ -10,24 +10,24 @@ use pocketmine\event\player\PlayerMoveEvent;
 
 class BlockEffectListener implements Listener {
 
-    private BlockEffect $main;
+	private BlockEffect $main;
 
-    public function __construct(BlockEffect $main) {
-        $this->main = $main;
-    }
+	public function __construct(BlockEffect $main) {
+		$this->main = $main;
+	}
 
-    public function onBlock(PlayerMoveEvent $event) {
-        $player = $event->getPlayer();
-        foreach ($this->main->getConfig()->getAll() as $item => $parametre) {
-            $poisonBlock = $parametre['block'];
-            $block = $player->getWorld()->getBlock($event->getPlayer()->getPosition()->down());
-            $effect = $parametre['effect'];
-            $effect = explode(":", $effect);
-            $minute = $effect[1] * 20;
-            if ($poisonBlock == $block->getId() . ":" . $block->getMeta()) {
-                $potionEffect = new EffectInstance(EffectIdMap::getInstance()->fromId((int)$effect[0]), $minute, $effect[2], $parametre['visible']);
-                $player->getEffects()->add($potionEffect);
-            }
-        }
-    }
+	public function onBlock(PlayerMoveEvent $event) {
+		$player = $event->getPlayer();
+		foreach ($this->main->getConfig()->getAll() as $item => $parametre) {
+			$poisonBlock = $parametre['block'];
+			$block = $player->getWorld()->getBlock($event->getPlayer()->getPosition()->down());
+			$effect = $parametre['effect'];
+			$effect = explode(":", $effect);
+			$minute = $effect[1] * 20;
+			if ($poisonBlock == $block->getId() . ":" . $block->getMeta()) {
+				$potionEffect = new EffectInstance(EffectIdMap::getInstance()->fromId((int)$effect[0]), $minute, $effect[2], $parametre['visible']);
+				$player->getEffects()->add($potionEffect);
+			}
+		}
+	}
 }
